50e3a065f03ebb307c0d2b80fc9c69ad0512988b
efarran0 <165324744+efarran0@users.noreply.github.com>
2024-04-11 11:52:54 +0200

    Add files via upload

--- source/scraper.py
+++ source/scraper.py
@@ -1,4 +1,4 @@
-# S'importa la funciÃ³ get_top250_movies de main i la llibreria os
+# S'importa la funció get_top250_movies de main i la llibreria os
 from main import get_top250_movies
 import os
 
@@ -6,7 +6,7 @@ import os
 user_agent_list = ['Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36',
                    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36']
 
-# S'obtÃ© la data
+# S'obté la data
 data = get_top250_movies(user_agent_list)
 
 # Es remou el csv antic en cas d'existir
@@ -14,4 +14,5 @@ if os.path.exists('data.csv'):
     os.remove('data.csv')
 
 # S'exporta data en format csv
-data.to_csv('data.csv', index=False)
+data.to_csv('data.csv',
+            index=False)
